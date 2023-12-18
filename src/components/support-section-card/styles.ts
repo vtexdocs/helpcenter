d35589c50a09b902e92d1502d3316beabf314813
aliceoq <aoqb@cin.ufpe.br>
2023-12-18 12:24:56 -0300

    feat(support): create support section

---
+++ src/components/support-section-card/styles.ts
@@ -0,0 +1,118 @@
+import { SxStyleProp } from '@vtex/brand-ui'
+
+const cardContainer: SxStyleProp = {
+  flexDirection: 'column',
+  margin: '16px',
+  width: '290px',
+  height: '293px',
+  boxSizing: 'initial',
+  borderRadius: '8px',
+  border: '1px solid #E7E9EE',
+  transition: 'all 0.3s ease-out',
+
+  ':active, :hover': {
+    cursor: 'pointer',
+    backgroundColor: 'white',
+    borderColor: 'muted.2',
+    transition: 'all 0.3s ease-out',
+
+    '.titleContainer': {
+      backgroundColor: '#363F4C',
+    },
+
+    '.quickStartedContainer': {
+      borderColor: 'muted.2',
+    },
+  },
+}
+
+const titleContainer: SxStyleProp = {
+  alignItems: 'center',
+  flexDirection: 'column',
+  py: '16px',
+  backgroundColor: 'muted.0',
+  color: 'white',
+  transition: 'all 0.3s ease-out',
+  rowGap: '10px',
+  borderRadius: '8px 8px 0px 0px',
+}
+
+const infoContainer: SxStyleProp = {
+  py: '24px',
+  px: '24px',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textAlign: 'center',
+  backgroundColor: '#FEFEFE',
+  borderTopLeftRadius: '8px',
+  borderTopRightRadius: '8px',
+  height: '157px',
+}
+
+const icon: SxStyleProp = {
+  width: '24px',
+  height: 'auto',
+}
+
+const title: SxStyleProp = {
+  fontSize: '22px',
+  lineHeight: '32px',
+  fontWeight: '400',
+}
+
+const description: SxStyleProp = {
+  fontSize: '16px',
+  lineHeight: '22px',
+  fontWeight: '400',
+  color: 'muted.1',
+  minHeight: '44px',
+  overflow: 'hidden',
+}
+
+const quickStartedContainer: SxStyleProp = {
+  height: '56px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderTop: '1px solid #E7E9EE',
+  borderBottomLeftRadius: '8px',
+  borderBottomRightRadius: '8px',
+  transition: 'all 0.3s ease-out',
+
+  '.learnMoreText': {
+    transition: 'all 0.3s ease-out',
+  },
+
+  ':active, :hover': {
+    transition: 'all 0.3s ease-out',
+
+    '.learnMoreText': {
+      color: '#000711',
+      transition: 'all 0.3s ease-out',
+    },
+  },
+}
+
+const learnMoreText: SxStyleProp = {
+  fontSize: '16px',
+  lineHeight: '22px',
+  fontWeight: '400',
+  color: 'muted.0',
+}
+
+const accessPortal: SxStyleProp = {
+  columnGap: '5px',
+  alignItems: 'center',
+}
+
+export default {
+  cardContainer,
+  description,
+  icon,
+  titleContainer,
+  infoContainer,
+  quickStartedContainer,
+  learnMoreText,
+  title,
+  accessPortal,
+}
--- src/components/support-section/styles.ts
+++ src/components/support-section/styles.ts
@@ -1,40 +1,35 @@
 import { SxStyleProp } from '@vtex/brand-ui'
 
 const sectionContainer: SxStyleProp = {
-  width: ['100%', '100%', '100%', '70%'],
-  px: ['8px', '12px', '96px', '38px'],
-  paddingTop: ['0px', '80px'],
+  width: '100%',
+  paddingTop: ['28px', '54px', '54px', '80px'],
   paddingBottom: ['24px', '52px', '52px', '48px'],
+  px: ['32px', '64px', '64px', '128px', '160px', '256px'],
   margin: 'auto',
   borderBottom: '1px solid #e7e9ed',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
 }
 
-const innerContainer: SxStyleProp = {
-  padding: [' 0 32px', '32px 0'],
-  flexDirection: ['column', 'column', 'column', 'row'],
-}
-const contentImage: SxStyleProp = {
-  textAlign: 'right',
-  paddingLeft: '40px',
-}
 const title: SxStyleProp = {
-  fontSize: ['20px', '28px', '28px', '40px'],
+  fontSize: ['28px', '28px', '28px', '40px'],
   lineHeight: ['30px', '38px', '38px', '50px'],
   fontWeight: '400',
   color: '#4A4A4A',
+  mb: ['26px', '33px'],
 }
 
 const contentCards: SxStyleProp = {
   flexWrap: 'wrap',
   flexDirection: 'row',
-  justifyContent: 'space-between',
+  alignItems: 'center',
+  justifyContent: 'center',
   flex: '0 0 50%',
 }
 
 export default {
   sectionContainer,
-  contentImage,
-  innerContainer,
   contentCards,
   title,
 }
