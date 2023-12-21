81371b038140ec310de833e2015bc9b7b234b9f7
Pedro Vitor <pvc3@cin.ufpe.br>
2023-12-21 16:54:12 -0300

    fix: changes site map placeholder

--- src/utils/get-url.tsx
+++ src/utils/get-url.tsx
@@ -23,7 +23,7 @@ export const getSupportURL = () => {
 }
 
 export const getSiteMapURL = () => {
-  return 'https://help.vtex.com/en/support' // TODO: get site map
+  return 'https://help.vtex.com/sitemap'
 }
 
 export const getLinkedinURL = () => {
