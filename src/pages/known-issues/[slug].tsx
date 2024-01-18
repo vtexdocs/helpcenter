d7612ff5aa4be792e59f1b777ffe0006d54c2472
aliceoq <aoqb@cin.ufpe.br>
2024-01-18 14:28:00 -0300

    feat: add known issues slug

---
+++ src/pages/known-issues/[slug].tsx
@@ -0,0 +1,284 @@
+import Head from 'next/head'
+import { useEffect, useState, useContext, useRef } from 'react'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import { PHASE_PRODUCTION_BUILD } from 'next/constants'
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemoteSerializeResult } from 'next-mdx-remote'
+import remarkGFM from 'remark-gfm'
+import rehypeHighlight from 'rehype-highlight'
+import hljsCurl from 'highlightjs-curl'
+import remarkBlockquote from 'utils/remark_plugins/rehypeBlockquote'
+
+import remarkImages from 'utils/remark_plugins/plaiceholder'
+
+import { Box, Flex, Text } from '@vtex/brand-ui'
+
+import DocumentContextProvider from 'utils/contexts/documentContext'
+
+import Contributors from 'components/contributors'
+import OnThisPage from 'components/on-this-page'
+import { Item, TableOfContents } from '@vtexdocs/components'
+import Breadcrumb from 'components/breadcrumb'
+
+import getHeadings from 'utils/getHeadings'
+import getNavigation from 'utils/getNavigation'
+// import getGithubFile from 'utils/getGithubFile'
+import { getDocsPaths as getTracksPaths } from 'utils/getDocsPaths'
+import escapeCurlyBraces from 'utils/escapeCurlyBraces'
+import replaceHTMLBlocks from 'utils/replaceHTMLBlocks'
+import { PreviewContext } from 'utils/contexts/preview'
+
+import styles from 'styles/documentation-page'
+import { ContributorsType } from 'utils/getFileContributors'
+
+import { getLogger } from 'utils/logging/log-util'
+import { localeType } from 'utils/navigation-utils'
+import { MarkdownRenderer } from '@vtexdocs/components'
+// import { ParsedUrlQuery } from 'querystring'
+import { useIntl } from 'react-intl'
+import { remarkReadingTime } from 'utils/remark_plugins/remarkReadingTime'
+import { getDocsPaths as getKnownIssuesPaths } from 'utils/getDocsPaths'
+import { getMessages } from 'utils/get-messages'
+
+const docsPathsGLOBAL = await getKnownIssuesPaths('known-issues')
+
+interface Props {
+  sectionSelected: string
+  breadcrumbList: { slug: string; name: string; type: string }[]
+  content: string
+  serialized: MDXRemoteSerializeResult
+  contributors: ContributorsType[]
+  path: string
+  headingList: Item[]
+  branch: string
+}
+
+const KnownIssuePage: NextPage<Props> = ({
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-ignore
+  serialized,
+  headingList,
+  contributors,
+  breadcrumbList,
+  branch,
+}) => {
+  const [headings, setHeadings] = useState<Item[]>([])
+  const { setBranchPreview } = useContext(PreviewContext)
+  const intl = useIntl()
+  setBranchPreview(branch)
+  const articleRef = useRef<HTMLElement>(null)
+
+  useEffect(() => {
+    setHeadings(headingList)
+  }, [serialized.frontmatter])
+
+  return (
+    <>
+      <Head>
+        <title>{serialized.frontmatter?.title as string}</title>
+        <meta name="docsearch:doctype" content="Start here" />
+        {serialized.frontmatter?.hidden && (
+          <meta name="robots" content="noindex" />
+        )}
+        {serialized.frontmatter?.excerpt && (
+          <meta
+            property="og:description"
+            content={serialized.frontmatter?.excerpt as string}
+          />
+        )}
+      </Head>
+      <DocumentContextProvider headings={headings}>
+        <Flex sx={styles.innerContainer}>
+          <Box sx={styles.articleBox}>
+            <Box sx={styles.contentContainer}>
+              <article ref={articleRef}>
+                <header>
+                  <Flex sx={styles.flexContainer}>
+                    <Breadcrumb breadcrumbList={breadcrumbList} />
+                    <Text sx={styles.readingTime}>
+                      {intl.formatMessage(
+                        {
+                          id: 'documentation_reading_time.text',
+                          defaultMessage: '',
+                        },
+                        { minutes: serialized.frontmatter?.readingTime }
+                      )}
+                    </Text>
+                  </Flex>
+                  <Text sx={styles.documentationTitle} className="title">
+                    {serialized.frontmatter?.title}
+                  </Text>
+                  <Text sx={styles.documentationExcerpt}>
+                    {serialized.frontmatter?.excerpt}
+                  </Text>
+                </header>
+                <MarkdownRenderer serialized={serialized} />
+              </article>
+            </Box>
+
+            <Box sx={styles.bottomContributorsContainer}>
+              <Box sx={styles.bottomContributorsDivider} />
+              <Contributors contributors={contributors} />
+            </Box>
+          </Box>
+          <Box sx={styles.rightContainer}>
+            <Contributors contributors={contributors} />
+            <TableOfContents headingList={headings} />
+          </Box>
+          <OnThisPage />
+        </Flex>
+      </DocumentContextProvider>
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  locale,
+  preview,
+  previewData,
+}) => {
+  const previewBranch =
+    preview && JSON.parse(JSON.stringify(previewData)).hasOwnProperty('branch')
+      ? JSON.parse(JSON.stringify(previewData)).branch
+      : 'main'
+  const branch = preview ? previewBranch : 'main'
+  const slug = params?.slug as string
+  const currentLocale: localeType = locale
+    ? (locale as localeType)
+    : ('en' as localeType)
+  const docsPaths =
+    process.env.NEXT_PHASE === PHASE_PRODUCTION_BUILD
+      ? docsPathsGLOBAL
+      : await getTracksPaths('tracks', branch)
+
+  const logger = getLogger('Start here')
+
+  const path = docsPaths[slug].find((e) => e.locale === currentLocale)?.path
+
+  if (!path) {
+    return {
+      notFound: true,
+    }
+  }
+
+  let documentationContent =
+    (await fetch(
+      `https://raw.githubusercontent.com/vtexdocs/help-center-content/${branch}/${path}`
+    )
+      .then((res) => res.text())
+      .catch((err) => console.log(err))) || ''
+
+  const contributors =
+    (await fetch(
+      `https://github.com/vtexdocs/help-center-content/file-contributors/${branch}/${path}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then(({ users }) => {
+        const result: ContributorsType[] = []
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        for (let i = 0; i < users.length; i++) {
+          const user = users[i]
+          if (user.id === '41898282') continue
+          result.push({
+            name: user.login,
+            login: user.login,
+            avatar: user.primaryAvatarUrl,
+            userPage: `https://github.com${user.profileLink}`,
+          })
+        }
+
+        return result
+      })
+      .catch((err) => console.log(err))) || []
+
+  let format: 'md' | 'mdx' = 'mdx'
+  try {
+    if (path.endsWith('.md')) {
+      documentationContent = escapeCurlyBraces(documentationContent)
+      documentationContent = replaceHTMLBlocks(documentationContent)
+    }
+  } catch (error) {
+    logger.error(`${error}`)
+    format = 'md'
+  }
+
+  try {
+    const headingList: Item[] = []
+    let serialized = await serialize(documentationContent, {
+      parseFrontmatter: true,
+      mdxOptions: {
+        remarkPlugins: [
+          remarkGFM,
+          remarkImages,
+          [getHeadings, { headingList }],
+          remarkBlockquote,
+          remarkReadingTime,
+        ],
+        rehypePlugins: [
+          [rehypeHighlight, { languages: { hljsCurl }, ignoreMissing: true }],
+        ],
+        format,
+      },
+    })
+
+    const sidebarfallback = await getNavigation()
+    serialized = JSON.parse(JSON.stringify(serialized))
+
+    logger.info(`Processing ${slug}`)
+    const seeAlsoData: {
+      url: string
+      title: string
+      category: string
+    }[] = []
+
+    const breadcrumbList: { slug: string; name: string; type: string }[] = [
+      {
+        slug: '/docs/known-issues/',
+        name: getMessages()[currentLocale]['known_issues_page.title'],
+        type: 'category',
+      },
+      {
+        slug,
+        name: serialized?.frontmatter?.title ?? '',
+        type: '',
+      },
+    ]
+
+    return {
+      props: {
+        slug,
+        serialized,
+        sidebarfallback,
+        headingList,
+        contributors,
+        path,
+        seeAlsoData,
+        breadcrumbList,
+        branch,
+      },
+      revalidate: 600,
+    }
+  } catch (error) {
+    logger.error(`Error while processing ${path}\n${error}`)
+    return {
+      notFound: true,
+    }
+  }
+}
+
+export default KnownIssuePage
