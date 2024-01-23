6588d9ca0e07cf1084cba06ec20f71c7585fca0c
aliceoq <aoqb@cin.ufpe.br>
2024-01-23 15:11:22 -0300

    feat: add select component

---
+++ src/components/select/index.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import styles from './styles'
+import { Flex } from '@vtex/brand-ui'
+
+interface Props {
+  options: { value: string; content: string }[]
+  onSelect: (value: string) => void
+  value: string
+  label: string
+}
+
+const Select = ({ value, label, options, onSelect }: Props) => {
+  const [selectedValue, setSelectedValue] = useState(value ?? '')
+
+  useEffect(() => {
+    if (
+      selectedValue !== value &&
+      options.find(({ value }) => value === selectedValue)
+    )
+      setSelectedValue(value)
+  }, [value])
+
+  return (
+    <Flex sx={styles.selectContainer}>
+      <label htmlFor={label}>{label}</label>
+      <select
+        id={label}
+        style={styles.select}
+        placeholder="teste"
+        onChange={(e) => {
+          console.log(e.currentTarget.value)
+          setSelectedValue(e.currentTarget.value)
+          onSelect(e.currentTarget.value)
+        }}
+        value={selectedValue}
+      >
+        <option hidden></option>
+        {options.map(({ value, content }, index) => (
+          <option key={index} value={value}>
+            {content}
+          </option>
+        ))}
+      </select>
+    </Flex>
+  )
+}
+
+export default Select
---
+++ src/components/select/styles.ts
@@ -0,0 +1,19 @@
+import { SxStyleProp } from '@vtex/brand-ui'
+
+const selectContainer: SxStyleProp = {
+  flexWrap: 'wrap',
+  columnGap: '16px',
+  rowGap: '8px',
+  alignItems: 'center',
+  fontSize: '14px',
+}
+
+const select: SxStyleProp = {
+  borderRadius: '4px',
+  padding: '0px 4px',
+  height: '100%',
+  fontSize: '14px',
+  border: '1px solid #E7E9EE',
+}
+
+export default { selectContainer, select }
