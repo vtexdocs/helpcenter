15403c9b829850bd17b79cf827e55f0a5b9cf64a
aliceoq <aoqb@cin.ufpe.br>
2024-02-05 20:24:39 -0300

    feat: add new announcement card

--- src/components/announcement-card/index.tsx
+++ src/components/announcement-card/index.tsx
@@ -1,88 +1,38 @@
-import Link from 'next/link'
-import { Box, Flex, Text, Timeline } from '@vtex/brand-ui'
+import { Box, Text, Link, Flex } from '@vtex/brand-ui'
 
-import { getDaysElapsed } from '../../utils/get-days-elapsed'
-import { useIntl } from 'react-intl'
+import type { AnnouncementDataElement } from 'utils/typings/types'
 
 import styles from './styles'
-import MegaphoneIcon from 'components/icons/megaphone-icon'
-import NewIcon from 'components/icons/new-icon'
+import Tag from 'components/tag'
+import { useIntl } from 'react-intl'
 
-export interface CardProps {
-  title: string
-  description: string
-  date: Date
-  first?: boolean
-}
-
-const AnnouncementTimelineItem = ({
+const AnnouncementCard = ({
   title,
-  date,
-  first = false,
-}: CardProps) => {
+  createdAt,
+  url,
+}: AnnouncementDataElement) => {
   const intl = useIntl()
+  const date = new Date(createdAt)
+  const currentDate = new Date()
+  const sevenDaysAgo = new Date(currentDate)
+  sevenDaysAgo.setDate(currentDate.getDate() - 7)
+  const isNew = date >= sevenDaysAgo && date <= currentDate
 
   return (
-    <Flex sx={styles.releaseContainer}>
-      <Timeline.Event
-        sx={styles.timeLineBar}
-        title={
-          first ? (
-            <Text sx={styles.newTitle}>New</Text>
-          ) : (
-            <Box sx={styles.placeholder}></Box>
-          )
-        }
-        icon={first ? <NewIcon sx={styles.icon} /> : null}
-      >
-        <Text sx={styles.timelineTitle}>{title}</Text>
-        <Text sx={styles.content}>
-          {`${getDaysElapsed(date)} ${intl.formatMessage({
-            id: 'relese-note-days-elapsed',
-          })}`}
+    <Link sx={styles.link} href={`updates/announcements/${url}`}>
+      <Box sx={styles.container}>
+        <Text sx={styles.date}>{intl.formatDate(date)}</Text>
+        <Text sx={styles.title} className="title">
+          {title}
         </Text>
-      </Timeline.Event>
-    </Flex>
-  )
-}
-
-interface Props {
-  announcements: CardProps[]
-}
-
-const AnnouncementCard = ({ announcements }: Props) => {
-  const intl = useIntl()
-  return (
-    <Link href={'/announcements'} legacyBehavior>
-      <Flex sx={styles.cardContainer}>
-        <Box>
-          <Flex sx={styles.title}>
-            <MegaphoneIcon />
-            <Text>
-              {intl.formatMessage({
-                id: 'landing_page_announcements.title',
-              })}
-            </Text>
-          </Flex>
-          <Text sx={styles.description}>
-            {intl.formatMessage({
-              id: 'landing_page_announcements.description',
-            })}
-          </Text>
-        </Box>
-        <Box sx={styles.timelineContainer}>
-          {announcements.map((announcement, index) => {
-            return index === 0 ? (
-              <AnnouncementTimelineItem
-                key={index}
-                {...{ ...announcement, first: true }}
-              />
-            ) : (
-              <AnnouncementTimelineItem key={index} {...announcement} />
-            )
-          })}
-        </Box>
-      </Flex>
+        <Flex sx={styles.bottomContainer}>
+          {isNew && (
+            <Tag sx={styles.tag} color={'New'}>
+              New
+            </Tag>
+          )}
+        </Flex>
+      </Box>
     </Link>
   )
 }
