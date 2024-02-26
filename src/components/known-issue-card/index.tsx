4f7d2d661c605980df9e7f8bd4f123d0d67f9ae8
aliceoq <aoqb@cin.ufpe.br>
2024-02-26 14:11:44 -0300

    feat(known-issues): add status translation

--- src/components/known-issue-card/index.tsx
+++ src/components/known-issue-card/index.tsx
@@ -5,6 +5,7 @@ import type { KnownIssueDataElement } from 'utils/typings/types'
 import styles from './styles'
 import Tag from 'components/tag'
 import DateText from 'components/date-text'
+import { useIntl } from 'react-intl'
 
 const KnownIssueCard = ({
   title,
@@ -15,6 +16,7 @@ const KnownIssueCard = ({
   createdAt,
   updatedAt,
 }: KnownIssueDataElement) => {
+  const intl = useIntl()
   const createdAtDate = new Date(createdAt)
   const updatedAtDate = new Date(updatedAt)
 
@@ -22,7 +24,11 @@ const KnownIssueCard = ({
     <Link href={`known-issues/${slug}`}>
       <Flex sx={styles.container}>
         <Flex sx={styles.topContainer}>
-          <Tag color={status}>{status}</Tag>
+          <Tag color={status}>
+            {intl.formatMessage({
+              id: `known_issues_filter_status.${status.toLowerCase()}`,
+            })}
+          </Tag>
           <Text sx={styles.knownIssueModule} className="module">
             {module}
           </Text>
