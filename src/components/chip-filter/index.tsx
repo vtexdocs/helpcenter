03c6f9d695697832d4678cc78d64cfadf45203ff
phvm <pedrohvdm@gmail.com>
2024-08-06 16:18:33 -0300

    feat: chip component for filtering

--- src/components/chip-filter/index.tsx
+++ src/components/chip-filter/index.tsx
@@ -2,26 +2,57 @@ import styles from './styles'
 
 import { useIntl } from 'react-intl'
 
+import { useRef } from 'react'
+
 interface ChipFilterProps {
   filters: string[]
   handleChipClick: (option: string) => void
 }
 
-export default function ChipFilter({ filters }: ChipFilterProps) {
+export default function ChipFilter({
+  filters,
+  handleChipClick,
+}: ChipFilterProps) {
   const intl = useIntl()
 
+  const containerRef = useRef<HTMLDivElement | null>(null)
+
+  function handleLeftArrowClick() {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft += 50
+    }
+  }
+
+  function handleRightArrowClick() {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft -= 50
+    }
+  }
+
   return (
-    <div style={styles.chipsContainer}>
+    <div style={styles.chipsContainer} ref={containerRef}>
+      <button onClick={handleLeftArrowClick}>{`<`}</button>
       <div style={styles.optionsContainer}>
-        <button type="button" value="all" style={styles.chip}>
+        <button
+          type="button"
+          value="all"
+          style={styles.chip}
+          onClick={() => handleChipClick('')}
+        >
           {intl.formatMessage({ id: 'chip.all_results' })}
         </button>
         {filters.map((filter: string) => (
-          <button type="button" value={filter} style={styles.chip}>
+          <button
+            type="button"
+            value={filter}
+            style={styles.chip}
+            onClick={() => handleChipClick(filter)}
+          >
             {filter}
           </button>
         ))}
       </div>
+      <button onClick={handleRightArrowClick}>{`>`}</button>
     </div>
   )
 }
