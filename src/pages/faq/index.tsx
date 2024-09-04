ae4a9a9da8d391aeda125acf7500b4da25a5cfa9
phvm <pedrohvdm@gmail.com>
2024-09-04 14:33:26 -0300

    feat: display category amount for each category selected

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -95,6 +95,12 @@ const FaqPage: NextPage<Props> = ({ faqData, branch }) => {
     setFilters(copyFilters)
   }
 
+  function getCategoryAmount(category: string): number {
+    return faqData.filter((data) => {
+      return data.productTeam === category
+    }).length
+  }
+
   return (
     <>
       <Head>
@@ -141,7 +147,7 @@ const FaqPage: NextPage<Props> = ({ faqData, branch }) => {
             removeCategory={handleFilterRemoval}
             resetFilters={handleFilterReset}
             filters={filters}
-            selectedCategoryAmount={filteredResult.length}
+            getCategoryAmount={getCategoryAmount}
             categories={chipCategories}
             applyCategory={handleCategoriesSelection}
           />
