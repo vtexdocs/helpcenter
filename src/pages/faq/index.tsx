46f95ba1205fb19b68fa22284c98c182e5775f55
phvm <pedrohvdm@gmail.com>
2024-09-10 14:01:51 -0300

    refactor: applyed filters comes from parent not from state

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -133,6 +133,7 @@ const FaqPage: NextPage<Props> = ({ faqData, branch }) => {
         <Flex sx={styles.container}>
           <Flex sx={styles.optionsContainer}>
             <Filter
+              selectedCheckboxes={filters}
               checkBoxFilter={faqFilter(intl)}
               onApply={(newFilters) => handleFilterApply(newFilters.checklist)}
             />
