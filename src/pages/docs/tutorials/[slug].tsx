36084f6226d7edf249e0d47288eebf0ebbe7bb72
Pedro Antunes <47991446+PedroAntunesCosta@users.noreply.github.com>
2025-04-29 20:49:31 -0300

    feat(tutorials page): add mismatched slug/locale handling
    fix #202

--- src/pages/docs/tutorials/[slug].tsx
+++ src/pages/docs/tutorials/[slug].tsx
@@ -189,6 +189,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   const keyPath = getKeyByValue(flattenedSidebar, slug)
 
   if (!keyPath) {
+    console.log('KeyPath not found (if (!keyPath)')
     return {
       notFound: true,
     }
@@ -300,14 +301,32 @@ export const getServerSideProps: GetServerSideProps = async ({
     }
   }
 
+  const keypathLocale = keyPath.split('slug.')[1]
+
   const docsPaths =
     process.env.NEXT_PHASE === PHASE_PRODUCTION_BUILD
       ? docsPathsGLOBAL
       : await getTutorialsPaths('tutorials', branch)
 
-  const path = docsPaths[slug]?.find((e) => e.locale === locale)?.path
+  let path = ''
+  if (locale === keypathLocale) {
+    path = docsPaths[slug]?.find((e) => e.locale === locale)?.path
+  } else {
+    const keyPathWithoutLocale = keyPath.split('.slug')[0]
+    const localizedSlug =
+      flattenedSidebar[`${keyPathWithoutLocale}.slug.${locale}`]
+    if (localizedSlug) {
+      return {
+        redirect: {
+          destination: `/${locale}/docs/tutorials/${localizedSlug}`,
+          permanent: true,
+        },
+      }
+    }
+  }
 
   if (!path) {
+    console.log('Path not found (if (!path))')
     return {
       notFound: true,
     }
