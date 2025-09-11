788fd3d75a55a08b3c79308f3f53b600eb190b80
Pedro Antunes <47991446+PedroAntunesCosta@users.noreply.github.com>
2025-09-11 20:53:42 -0300

    feat(redirect edge function): implement broader edge function to handle all redirects from oldportal

--- netlify/edge-functions/redirect-from-old-portal.js
+++ netlify/edge-functions/redirect-from-old-portal.js
@@ -1,22 +1,40 @@
 export default async (request, context) => {
+  console.log('running edge function: redirect-from-old-portal')
+  console.log('request.url', request.url)
+
   const url = new URL(request.url)
 
-  console.log('running edge function')
-  console.log(url.pathname)
+  // Match patterns:
+  // /<locale>/{type}/{slug}[--<key>]
+  // /{type}/{slug}[--<key>]
+  const match = url.pathname.match(
+    /^(?:\/(?<locale>[a-z]{2}))?\/(?<type>tutorial|tracks|faq)\/(?<slug>[^/]+?)(?:--[^/]+)?$/
+  )
 
-  const match = url.pathname.match(/^\/([^/]+\/)?tutorial\/([^/]+?)(--[^/]+)?$/)
-
-  if (match) {
+  if (match && match.groups) {
     console.log('match found')
+    const locale = match.groups.locale || 'en' // default if no locale
+    const type = match.groups.type // "tutorial", "tracks", "faq"
+    const slug = match.groups.slug // clean slug without --key
+    const search = url.search || '' // preserve query string
+    console.log('locale', locale)
+    console.log('type', type)
+    console.log('slug', slug)
+    console.log('search', search)
 
-    const maybeLocale = match[1] || ''
-    const slug = match[2]
+    let destination
 
-    const locale = maybeLocale.replace('/', '') || 'en'
+    if (type === 'tutorial') {
+      destination = `/${locale}/docs/tutorials/${slug}`
+    } else if (type === 'tracks') {
+      destination = `/${locale}/docs/tracks/${slug}`
+    } else if (type === 'faq') {
+      destination = `/${locale}/faq/${slug}`
+    }
 
-    const destination = `/${locale}/docs/tutorials/${slug}`
+    console.log('destination', destination)
 
-    return Response.redirect(new URL(destination, url.origin), 308)
+    return Response.redirect(new URL(destination + search, url.origin), 308)
   }
 
   return context.next()
