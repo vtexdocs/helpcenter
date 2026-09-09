fcadf4b4bc10701ade42f8405070c6775fb1c24f
Bárbara Celi <barbara.celi@vtex.com>
2026-09-09 17:37:49 -0300

    chore(constants): move 'known issues' section

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -72,6 +72,18 @@ export const updatesData = (intl: IntlShape) => {
       }),
       link: '/announcements',
     },
+    {
+      id: 'known-issues',
+      Icon: WarningIcon,
+      title: intl.formatMessage({
+        id: 'sidebar_known_issues.title',
+      }),
+      description: intl.formatMessage({
+        id: 'sidebar_known_issues.description',
+      }),
+      link: '/known-issues',
+      excludeFromSearch: true,
+    },
   ]
   return data
 }
@@ -86,18 +98,6 @@ export const menuSupportData = (intl: IntlShape) => {
         id: 'updates_info_notes.description',
       }),
       link: '/faq',
-    },
-    {
-      id: 'known-issues',
-      Icon: WarningIcon,
-      title: intl.formatMessage({
-        id: 'sidebar_known_issues.title',
-      }),
-      description: intl.formatMessage({
-        id: 'sidebar_known_issues.description',
-      }),
-      link: '/known-issues',
-      excludeFromSearch: true,
     },
     {
       id: 'troubleshooting',
